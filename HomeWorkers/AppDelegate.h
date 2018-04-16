d3557347bf56ab147eae3013c0b0066532499442
apple <apple@apples-macbook-pro-2.local>
2018-04-16 10:05:48 +0530

    Initial Commit

---
+++ HomeWorkers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeWorkers
+//
+//  Created by apple on 16/04/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
